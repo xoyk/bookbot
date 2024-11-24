d6966f0a0267dc9604467ee541f477819607d862
xoyk <xoyk82@gmail.com>
2024-11-24 16:27:02 +0400

    add report generation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main(path_to_file):
-    text = get_book_text(path_to_file)
-    num_words = get_num_words(text)
-    print(num_words)
+    
+    report = generate_report(path_to_file)
+    print(report)
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -11,5 +11,28 @@ def get_book_text(path_to_file):
 def get_num_words(text):
     return len(text.split())
 
+def get_num_letters(text):
+    result = {}
+
+    for letter in text:
+        key = letter.lower()
+        if key in result:
+            result[key] += 1
+        else:
+            result[key] = 1
+        
+    return result
+
+def generate_report(path_to_file):
+    text = get_book_text(path_to_file)
+    num_words = get_num_words(text)
+    num_letters = get_num_letters(text)
+    report = f"--- Begin report of {path_to_file} ---\n"
+    report += f"{num_words} words found in the document\n\n"
+    for letter in num_letters:
+        report += f"The '{letter}' character was found {num_letters[letter]} times\n"
+
+    report += "--- End report ---"
+    return report
 
 main("books/frankenstein.txt")
